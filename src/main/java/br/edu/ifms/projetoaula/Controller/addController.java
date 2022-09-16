527ae53a84aea8fb9d7be6a78c6b2d904bed21eb
Gabriel9816 <gabriel.oliveira6@estudante.ifms.edu.br>
2022-09-16 19:28:19 -0400

    edit

--- src/main/java/br/edu/ifms/projetoaula/Controller/addController.java
+++ src/main/java/br/edu/ifms/projetoaula/Controller/addController.java
@@ -1,8 +1,9 @@
 package br.edu.ifms.projetoaula.Controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +15,13 @@ import br.edu.ifms.projetoaula.Domain.add;
 public class addController {
     
     @GetMapping("/{n1}/{n2}")
-    public ResponseEntity<Integer> add(@PathVariable("n1") int n1, @PathVariable("n2") int n2) {
-        add add_operation = new add(n1, n2);
-        return ResponseEntity.ok(add_operation.getResult());
+    public Integer add(@PathVariable("n1") int n1, @PathVariable("n2") int n2) {
+        return new add(n1, n2).getResult();
+    }
+
+
+    @PostMapping
+    public Integer addByPost(@RequestBody add Add) {
+        return Add.getResult();
     }
 }
--- src/main/java/br/edu/ifms/projetoaula/Controller/greetingController.java
+++ src/main/java/br/edu/ifms/projetoaula/Controller/greetingController.java
@@ -1,8 +1,11 @@
 package br.edu.ifms.projetoaula.Controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ifms.projetoaula.Domain.greeting;
@@ -10,9 +13,26 @@ import br.edu.ifms.projetoaula.Domain.greeting;
 @RestController
 @RequestMapping(value = "/greeting")
 public class greetingController {
+    
     @GetMapping
-    public ResponseEntity<String> sayHello() {
-        greeting greeting = new greeting("Hello, World!");
-        return ResponseEntity.ok(greeting.getGreeting());
+    public greeting sayHello() {
+        return new greeting("Hello, World!");
+    }
+  
+    @GetMapping("/{name}")
+    public greeting pathParam(@PathVariable("name") String name) {
+        return new greeting("Hello, " + name);
+    }
+
+
+    @GetMapping("/")
+    public greeting queryParam(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new greeting("Hello, " + name);
+    }
+
+
+    @PostMapping
+    public greeting postMethod(@RequestBody greeting greeting) {
+        return greeting;
     }
 }
